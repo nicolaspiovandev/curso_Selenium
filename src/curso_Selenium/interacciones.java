bc2c82fb28f0fc6e0422109115829ba1cb82a1eb
Nicolás Piován <nicolaspiovandev@gmail.com>
2023-08-30 18:53:39 +0200

    Se crean interacciones
    Se crean interacciones para reproducir pasos combinados en los 3 navegadores (Chrome, Firefox y Edge)

---
+++ src/curso_Selenium/interacciones.java
@@ -0,0 +1,59 @@
+package curso_Selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class interacciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//chrome
+		System.setProperty("webdriver.Chrome.driver", "C:\\Users\\Nico\\eclipse-workspace\\Drivers\\chromedriver\\chromedriver.exe");	
+		WebDriver driver_chrome = new ChromeDriver();
+		
+		driver_chrome.get("https://www.amazon.com");
+		//send keys 
+		driver_chrome.findElement(By.id("twotabsearchtextbox")).sendKeys("nintendo switch");
+	    driver_chrome.findElement(By.id("nav-search-submit-button")).click();
+		//clear
+		//driver_chrome.findElement(By.id("twotabsearchtextbox")).clear();
+		//click()
+		//driver_chrome.findElement(By.id("nav-hamburger-menu")).click();
+		//submit() ya no es valido
+		
+		
+		
+		//firefox
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Nico\\eclipse-workspace\\Drivers\\geckodriver_mozilla\\geckodriver.exe");	
+		WebDriver driver_firefox = new FirefoxDriver();		
+		
+		driver_firefox.get("https://www.amazon.com");
+		//send keys 
+		driver_firefox.findElement(By.id("twotabsearchtextbox")).sendKeys("nintendo switch");
+	    driver_firefox.findElement(By.id("nav-search-submit-button")).click();
+		//clear
+		//driver_firefox.findElement(By.id("twotabsearchtextbox")).clear();
+		//click()
+		//driver_firefox.findElement(By.id("nav-hamburger-menu")).click();
+		//submit() ya no es valido
+		
+		
+		// edge
+		//System.setProperty("webdriver.edge.driver", "C:\\Users\\Nico\\eclipse-workspace\\Drivers\\msedgedriver\\msedgedriver.exe");	
+		//WebDriver driver_edge = new EdgeDriver();
+		
+		driver_edge.get("https://www.amazon.com");
+		//send keys 
+		driver_edge.findElement(By.id("twotabsearchtextbox")).sendKeys("nintendo switch");
+	    driver_edge.findElement(By.id("nav-search-submit-button")).click();
+		//clear
+		//driver_edge.findElement(By.id("twotabsearchtextbox")).clear();
+		//click()
+		//driver_edge.findElement(By.id("nav-hamburger-menu")).click();
+		//submit() ya no es valido
+	}
+
+}
